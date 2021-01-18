f509778a4786b6d8d519b6bb29175e80338d38db
Malcolm St. John <malcolm19@hotmail.com>
2021-01-18 17:52:58 -0500

    Printing received data body

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	// Handle func is a convenience method on the http package
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	// Handle func is a convenience method on the http package.
+	// Registers a function to a path on the default serve mux (http handler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
+		data, _ := ioutil.ReadAll(r.Body)
+		log.Printf("Data %s\n", data)
 	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye")
+	})
+
 	// All that is required to run a web service
 	http.ListenAndServe(":9090", nil)
 }
