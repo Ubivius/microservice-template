ef4a09f0e350743c7c7fe4a537fd3da796bbfabe
Malcolm St. John <malcolm19@hotmail.com>
2021-01-21 11:38:48 -0500

    Added second handller

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ import (
 func main() {
 	l := log.New(os.Stdout, "microservice-prototype", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	achievementHandlers := handlers.NewAchievement(l)
 	gorillaMux := mux.NewRouter()
 	gorillaMux.HandleFunc("/", hh.ServeHTTP)
-
-	sm := http.NewServeMux()
-	sm.Handle("/", hh)
+	gorillaMux.HandleFunc("/achievement", achievementHandlers.ServeHTTP)
+	http.Handle("/", gorillaMux)
 
 	// All that is required to run a web service
-	http.ListenAndServe(":9090", sm)
+	http.ListenAndServe(":9090", nil)
 }
