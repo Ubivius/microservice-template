a1ae7faaa4042b2f8755f3a50988e95d9ba3df04
Malcolm St. John <malcolm19@hotmail.com>
2021-01-21 13:19:06 -0500

    Example of db connection, should be moved out of main

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.15
 replace handlers => ./handlers
 
 require (
+	github.com/elastic/go-elasticsearch v0.0.0
+	github.com/elastic/go-elasticsearch/v8 v8.0.0-20201229214741-2366c2514674 // indirect
 	github.com/gorilla/mux v1.8.0
 	handlers v1.0.0
 )
--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"handlers"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/elastic/go-elasticsearch"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	//Logger
 	l := log.New(os.Stdout, "microservice-prototype", log.LstdFlags)
+
+	// Configuration elastic search
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://localhost:9200",
+			"http://localhost:9201",
+		}
+	}
+
+	es, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		l.Println("Error creating the es client.")
+	}
+
+	var b bytes.Buffer
+	b.WriteString(`{"Users" : "Jeremi"}`)
+
+	res, _ := es.Index("method1", &b)
+
+	// Handlers
 	helloHandler := handlers.NewHello(l)
 	achievementHandlers := handlers.NewAchievement(l)
+
+	// Routing
 	gorillaMux := mux.NewRouter()
 	gorillaMux.HandleFunc("/", helloHandler.ServeHTTP)
 	gorillaMux.HandleFunc("/achievement", achievementHandlers.ServeHTTP)
 
-	// All that is required to run a web service
+	// Start server
 	http.ListenAndServe(":9090", gorillaMux)
 }
