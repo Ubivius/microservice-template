90e36b7aa08e8f922115a63d73fd3d5c6850da75
Malcolm St. John <malcolm19@hotmail.com>
2021-01-21 11:04:52 -0500

    fixed handler creation

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello()
+	hh := handlers.NewHello(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
