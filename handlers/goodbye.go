2c5366ddb734fcfcb655ecba9fdea1d29296881c
Malcolm St. John <malcolm19@hotmail.com>
2021-01-21 16:21:05 -0500

    Added new server

---
+++ handlers/goodbye.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+type Goodbye struct {
+	l *log.Logger
+}
+
+func NewGoodbye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
+func (g *Goodbye) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Bye"))
+}
--- main.go
+++ main.go
@@ -5,15 +5,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
+	// Logger
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello()
 
+	// Creating handlers
+	helloHandler := handlers.NewHello(l)
+	goodbyeHandler := handlers.NewGoodbye(l)
+
+	// Mux route handling with default http ServeMux
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
+	sm.Handle("/", helloHandler)
+	sm.Handle("/goodbye", goodbyeHandler)
 
-	// All that is required to run a web service
-	http.ListenAndServe(":9090", sm)
+	// Server setup
+	server := &http.Server{
+		Addr:        ":9090",
+		Handler:     sm,
+		IdleTimeout: 120 * time.Second,
+		ReadTimeout: 1 * time.Second,
+	}
+	server.ListenAndServe()
 }
