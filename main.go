9c1beda62b02db85b2171d402afe8950d14412d5
Malcolm St. John <malcolm19@hotmail.com>
2021-01-21 08:27:34 -0500

    Moved handler to new directory

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -11,14 +9,7 @@ func main() {
 	// Handle func is a convenience method on the http package.
 	// Registers a function to a path on the default serve mux (http handler)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		data, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Oops", http.StatusBadRequest)
-			return
-		}
-		log.Printf("Data : %s\n", data)
-		fmt.Fprintf(w, "Hello %s", data)
+
 	})
 
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
