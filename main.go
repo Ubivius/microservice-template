a540c88d1174adb469136a64a272f040e8ea2858
Malcolm St. John <malcolm19@hotmail.com>
2021-01-21 11:46:43 -0500

    Added routing with gorilla/mux

--- main.go
+++ main.go
@@ -11,13 +11,12 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "microservice-prototype", log.LstdFlags)
-	hh := handlers.NewHello(l)
+	// hh := handlers.NewHello(l)
 	achievementHandlers := handlers.NewAchievement(l)
 	gorillaMux := mux.NewRouter()
-	gorillaMux.HandleFunc("/", hh.ServeHTTP)
+	gorillaMux.HandleFunc("/", handlers.NewHello(l).ServeHTTP)
 	gorillaMux.HandleFunc("/achievement", achievementHandlers.ServeHTTP)
-	http.Handle("/", gorillaMux)
 
 	// All that is required to run a web service
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", gorillaMux)
 }
