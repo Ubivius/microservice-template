7bd6433bd432db507b6d3ffa2fe8f775b40d3073
Malcolm St. John <malcolm19@hotmail.com>
2021-01-21 20:32:12 -0500

    Created initial products file without any json or encoding references

---
+++ data/products.go
@@ -0,0 +1,43 @@
+package data
+
+import (
+	"time"
+)
+
+// Product defines the structure for an API product
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+// Products is a collection of Product
+type Products []*Product
+
+// productList is a hard coded list of products for this
+// example data source
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
