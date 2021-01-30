0fd9a4cc98c61eed12a3fbf80d1480c198ccbb3c
Malcolm St. John <malcolm19@hotmail.com>
2021-01-30 16:48:08 -0500

    added comments to validate file

--- data/validate.go
+++ data/validate.go
@@ -6,6 +6,12 @@ import (
 	"github.com/go-playground/validator"
 )
 
+// The current setup works well with a single struct to validate
+// The struct to validate should be passed as an interface in the future and the errors should be handled as individual error strings
+// For further information see :
+// Validator library : https://github.com/go-playground/validator
+// Nic Jackson episode : https://github.com/nicholasjackson/building-microservices-youtube/blob/episode_7/product-api/data/validation.go
+
 // ValidateProduct a product with json validation and customer SKU validator
 func (p *Product) ValidateProduct() error {
 	validate := validator.New()
