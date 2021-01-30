216bb149c00b9f6d88dcd5150a220d566876cc82
Malcolm St. John <malcolm19@hotmail.com>
2021-01-30 18:59:03 -0500

    Removed product specific json encoding and decoding

--- data/json.go
+++ data/json.go
@@ -5,18 +5,6 @@ import (
 	"io"
 )
 
-// FromProductJSON deserializes product from JSON string
-func (product *Product) FromProductJSON(reader io.Reader) error {
-	decoder := json.NewDecoder(reader)
-	return decoder.Decode(product)
-}
-
-// ToProductJSON serializes products into a json String
-func (products *Products) ToProductJSON(w io.Writer) error {
-	encoder := json.NewEncoder(w)
-	return encoder.Encode(products)
-}
-
 // FromProductJSON deserializes the interface from JSON string
 func FromJSON(i interface{}, reader io.Reader) error {
 	decoder := json.NewDecoder(reader)
