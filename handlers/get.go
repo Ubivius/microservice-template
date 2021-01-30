79542b5e0e273c96206b81c1b9395042305e59bb
Malcolm St. John <malcolm19@hotmail.com>
2021-01-30 18:11:52 -0500

    GET all

--- handlers/get.go
+++ handlers/get.go
@@ -6,11 +6,14 @@ import (
 	"github.com/Ubivius/microservice-template/data"
 )
 
+// GET /products
+// Returns the full list of products
 func (productHandler *ProductsHandler) GetProducts(responseWriter http.ResponseWriter, request *http.Request) {
 	productHandler.logger.Println("Handle GET products")
 	productList := data.GetProducts()
-	err := productList.ToProductJSON(responseWriter)
+	err := data.ToJSON(productList, responseWriter)
 	if err != nil {
+		productHandler.logger.Println("[ERROR] serializing product", err)
 		http.Error(responseWriter, "Unable to marshal json", http.StatusInternalServerError)
 	}
 }
