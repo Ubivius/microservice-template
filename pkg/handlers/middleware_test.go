c3367a57141b7b313e476ca0cd8baae454bd4ffe
Malcolm St. John <malcolm19@hotmail.com>
2021-02-17 16:15:20 -0500

    Using a router for middleware tests

--- pkg/handlers/middleware_test.go
+++ pkg/handlers/middleware_test.go
@@ -1,36 +1,38 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Ubivius/microservice-template/pkg/data"
+	"github.com/gorilla/mux"
 )
 
-// Creating simple http handler to pass to middleware
-type HttpHandler struct{}
+func TestValidationMiddleware(t *testing.T) {
+	// Create a router for middleware because function attachment is handled by gorilla/mux
+	router := mux.NewRouter()
 
-func (h HttpHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
-	responseWriter.WriteHeader(http.StatusNoContent)
+	// Creating request body
+	body := &data.Product{
+		Name:        "addName",
+		Description: "addDescription",
+		Price:       1,
+		SKU:         "abc-abc-abcd",
+	}
+
+	request := httptest.NewRequest(http.MethodPost, "/products", nil)
+	response := httptest.NewRecorder()
+
+	// Add the body to the context since we arent passing through middleware
+	ctx := context.WithValue(request.Context(), KeyProduct{}, body)
+	newRequest := request.WithContext(ctx)
+
+	productHandler := NewProductsHandler(NewTestLogger())
+	productHandler.AddProduct(response, newRequest)
+
+	if response.Code != http.StatusNoContent {
+		t.Errorf("Expected status code %d, but got %d", http.StatusNoContent, response.Code)
+	}
 }
-
-// func TestValidationMiddleware(t *testing.T) {
-// 	// Creating handler to pass to middleware validator
-// 	handler := &HttpHandler{}
-// 	// Creating request body
-// 	body := &data.Product{
-// 		Name:        "addName",
-// 		Description: "addDescription",
-// 		Price:       1,
-// 		SKU:         "abc-abc-abcd",
-// 	}
-
-// 	bodyBytes, _ := json.Marshal(body)
-
-// 	request := httptest.NewRequest(http.MethodPost, "/products", strings.NewReader(string(bodyBytes)))
-// 	response := httptest.NewRecorder()
-
-// 	productHandler := NewProductsHandler(NewTestLogger())
-// 	productHandler.MiddlewareProductValidation(handler)
-
-// 	if response.Code != http.StatusNoContent {
-// 		t.Errorf("Expected status code %d, but got %d", http.StatusNoContent, response.Code)
-// 	}
-// }
