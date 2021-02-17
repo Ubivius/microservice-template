35ecb8eb833ee158d176dc8f259a5896568db110
Malcolm St. John <malcolm19@hotmail.com>
2021-02-17 16:07:26 -0500

    Commented out request example

--- pkg/handlers/middleware_test.go
+++ pkg/handlers/middleware_test.go
@@ -1,1 +1,36 @@
 package handlers
+
+import (
+	"net/http"
+)
+
+// Creating simple http handler to pass to middleware
+type HttpHandler struct{}
+
+func (h HttpHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.WriteHeader(http.StatusNoContent)
+}
+
+// func TestValidationMiddleware(t *testing.T) {
+// 	// Creating handler to pass to middleware validator
+// 	handler := &HttpHandler{}
+// 	// Creating request body
+// 	body := &data.Product{
+// 		Name:        "addName",
+// 		Description: "addDescription",
+// 		Price:       1,
+// 		SKU:         "abc-abc-abcd",
+// 	}
+
+// 	bodyBytes, _ := json.Marshal(body)
+
+// 	request := httptest.NewRequest(http.MethodPost, "/products", strings.NewReader(string(bodyBytes)))
+// 	response := httptest.NewRecorder()
+
+// 	productHandler := NewProductsHandler(NewTestLogger())
+// 	productHandler.MiddlewareProductValidation(handler)
+
+// 	if response.Code != http.StatusNoContent {
+// 		t.Errorf("Expected status code %d, but got %d", http.StatusNoContent, response.Code)
+// 	}
+// }
