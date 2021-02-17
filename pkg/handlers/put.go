e7d2cfeae6f44b4681e3a3a63981e0c58800c470
Malcolm St. John <malcolm19@hotmail.com>
2021-02-17 15:39:57 -0500

    made put accept a pointer instead of sending address with & to function

--- pkg/handlers/put.go
+++ pkg/handlers/put.go
@@ -8,11 +8,11 @@ import (
 
 // UpdateProducts updates the product with the ID specified in the received JSON product
 func (productHandler *ProductsHandler) UpdateProducts(responseWriter http.ResponseWriter, request *http.Request) {
-	product := request.Context().Value(KeyProduct{}).(data.Product)
+	product := request.Context().Value(KeyProduct{}).(*data.Product)
 	productHandler.logger.Println("Handle PUT product", product.ID)
 
 	// Update product
-	err := data.UpdateProduct(&product)
+	err := data.UpdateProduct(product)
 	if err == data.ErrorProductNotFound {
 		productHandler.logger.Println("[ERROR} product not found", err)
 		http.Error(responseWriter, "Product not found", http.StatusNotFound)
