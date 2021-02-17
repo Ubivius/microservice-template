4022fff2f64fbe2d6277780ce3a536265636a0a4
Malcolm St. John <malcolm19@hotmail.com>
2021-02-17 16:22:28 -0500

    Middleware test working with valid request body

--- pkg/handlers/middleware_test.go
+++ pkg/handlers/middleware_test.go
@@ -11,12 +11,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func TestValidationMiddleware(t *testing.T) {
-	productHandler := NewProductsHandler(NewTestLogger())
-	// Create a router for middleware because function attachment is handled by gorilla/mux
-	router := mux.NewRouter()
-	router.HandleFunc("/products").Use(productHandler.MiddlewareProductValidation)
-
+func TestValidationMiddlewareWithValidBody(t *testing.T) {
 	// Creating request body
 	body := &data.Product{
 		Name:        "addName",
@@ -28,6 +23,16 @@ func TestValidationMiddleware(t *testing.T) {
 
 	request := httptest.NewRequest(http.MethodPost, "/products", strings.NewReader(string(bodyBytes)))
 	response := httptest.NewRecorder()
+
+	productHandler := NewProductsHandler(NewTestLogger())
+
+	// Create a router for middleware because function attachment is handled by gorilla/mux
+	router := mux.NewRouter()
+	router.HandleFunc("/products", productHandler.AddProduct)
+	router.Use(productHandler.MiddlewareProductValidation)
+
+	// Server http on our router
+	router.ServeHTTP(response, request)
 
 	if response.Code != http.StatusNoContent {
 		t.Errorf("Expected status code %d, but got %d", http.StatusNoContent, response.Code)
