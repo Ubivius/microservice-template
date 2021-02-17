b60b8a0fe518c0555df461c5ffa8389d65033ca3
Malcolm St. John <malcolm19@hotmail.com>
2021-02-17 12:57:20 -0500

    Curl requests updated

--- scripts/curlRequests.sh
+++ scripts/curlRequests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 curl localhost:9090/products
-curl localhost:9090/products -XPOST --header "Content-Type: application/json" -d '{"name":"addName", "price":1.00, "sku":"abc-abc-abcd"}'
-curl localhost:9090/products -XPOST --header "Content-Type: application/json" -d "{\"name\":\"addName\", \"price\":1.00, \"sku\":\"abc-abc-abcd\"}"
 curl localhost:9090/products/1
-curl localhost:9090/products/3
+curl localhost:9090/products -XPOST --header "Content-Type: application/json" -d '{"name":"addName", "price":1.00, "sku":"abc-abc-abcd"}'
+curl localhost:9090/products
 curl localhost:9090/delete/1
-curl localhost:9090/products -XPUT -d '{"id":1, "name":"addName", "price":1.00, "sku":"abc-abc-abcd"}'
+curl localhost:9090/products
+curl localhost:9090/products -XPUT -d '{"id":2, "name":"newName", "price":1.00, "sku":"abc-abc-abcd"}'
