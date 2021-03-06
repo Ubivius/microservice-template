252127d6c532262875efde80e090898c4422e8be
JeremiSavard <jeremi.savard@usherbrooke.ca>
2021-03-06 15:40:16 -0500

    SignUp and SignIn

--- authentication/authenticationMiddleware.go
+++ authentication/authenticationMiddleware.go
@@ -1,11 +1,14 @@
 package authentication
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"regexp"
 	"strings"
 
 	"github.com/coreos/go-oidc"
@@ -30,7 +33,7 @@ func init() {
 	}
 
 	clientID := "ubivius-client"
-	clientSecret := "ef14f638-98ad-4c5b-9320-a223077e0797"
+	clientSecret := "7d109d2b-524f-4351-bfda-44ecad030eef"
 
 	redirectURL := "http://localhost:9090/ubivius/callback"
 	// Configure an OpenID Connect aware OAuth2 client.
@@ -59,7 +62,9 @@ func Middleware(next http.Handler) http.Handler {
 		log.Println(rawAccessToken)
 		if rawAccessToken == "" {
 			log.Println("No access token provided")
-			http.Redirect(w, r, oauth2Config.AuthCodeURL(state), http.StatusFound)
+			//http.Redirect(w, r, oauth2Config.AuthCodeURL(state), http.StatusFound)
+			w.WriteHeader(http.StatusForbidden)
+			w.Write([]byte("status code 403"))
 			return
 		}
 
@@ -170,4 +175,98 @@ func RedirectToInitialUrl(accessToken string) {
 		log.Println("Error while reading the response bytes:", err)
 	}
 	log.Println(string([]byte(body)))
+}
+
+func SignIn(username string, password string) {
+	urlPath := "http://localhost:8080/auth/realms/ubivius/protocol/openid-connect/token"
+
+	data := url.Values{}
+	data.Set("client_id", "ubivius-client")
+	data.Set("grant_type", "password")
+	data.Set("client_secret", "7d109d2b-524f-4351-bfda-44ecad030eef")
+	data.Set("scope", "openid")
+	data.Set("username", username)
+	data.Set("password", password)
+
+	req, err := http.NewRequest("POST", urlPath, strings.NewReader(data.Encode()))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	log.Println("response Status:", resp.Status)
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Println("response Body:", string(body))
+}
+
+func SignUp(firstName string, lastName string, email string, username string) {
+	urlPath := "http://localhost:8080/auth/admin/realms/ubivius/users"
+
+	values := map[string]string{"firstName": firstName, "lastName": lastName, "email": email, "username": username, "enabled": "true"}
+	jsonValues, _ := json.Marshal(values)
+
+	req, err := http.NewRequest("POST", urlPath, bytes.NewBuffer(jsonValues))
+	if err != nil {
+		panic(err)
+	}
+	admin_token := GetAdminAccessToken()
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+admin_token)
+	log.Println(req.Header)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	log.Println("response Status:", resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	log.Println("response Body:", string(body))
+}
+
+func GetAdminAccessToken() string {
+	urlPath := "http://localhost:8080/auth/realms/ubivius/protocol/openid-connect/token"
+
+	data := url.Values{}
+	data.Set("client_id", "ubivius-client")
+	data.Set("grant_type", "client_credentials")
+	data.Set("client_secret", "7d109d2b-524f-4351-bfda-44ecad030eef")
+
+	req, err := http.NewRequest("POST", urlPath, strings.NewReader(data.Encode()))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	log.Println("Admintoken response Status:", resp.Status)
+	body, _ := ioutil.ReadAll(resp.Body)
+	admin_token := extractValue(string(body), "access_token")
+	return admin_token
+}
+
+// extracts the value for a key from a JSON-formatted string
+// body - the JSON-response as a string. Usually retrieved via the request body
+// key - the key for which the value should be extracted
+// returns - the value for the given key
+func extractValue(body string, key string) string {
+	keystr := "\"" + key + "\":[^,;\\]}]*"
+	r, _ := regexp.Compile(keystr)
+	match := r.FindString(body)
+	keyValMatch := strings.Split(match, ":")
+	return strings.ReplaceAll(keyValMatch[1], "\"", "")
 }
