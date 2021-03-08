3a4fca29803dd4970ef112e0598ad12cbf0f3bc5
Malcolm St. John <malcolm19@hotmail.com>
2021-03-08 17:59:31 -0500

    Added get products functions

--- pkg/database/products_mongo.go
+++ pkg/database/products_mongo.go
@@ -1,4 +1,18 @@
 package database
 
+import "github.com/Ubivius/microservice-template/pkg/data"
+
 type MongoProducts struct {
+}
+
+func NewMongoProducts() *MongoProducts {
+	return &MongoProducts{}
+}
+
+func (mp *MongoProducts) GetProducts() data.Products {
+	return nil
+}
+
+func (mp *MongoProducts) GetProductByID() (*data.Product, error) {
+	return &data.Product{}, nil
 }
