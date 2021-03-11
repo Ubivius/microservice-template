ab3265fba102c1a9f41e5a460c6829b0174b491f
Malcolm St. John <malcolm19@hotmail.com>
2021-03-10 22:02:46 -0500

    Added comments to test and a shutdown test

--- pkg/database/mongoDB_test.go
+++ pkg/database/mongoDB_test.go
@@ -2,9 +2,16 @@ package database
 
 import "testing"
 
+// Should be an integration test
 func TestMongoDBConnection(t *testing.T) {
 	mp := NewMongoProducts()
 	if mp == nil {
 		t.Fail()
 	}
+}
+
+// Temporary test
+func TestMongoDBConnectionAndShutdown(t *testing.T) {
+	mp := NewMongoProducts()
+	mp.CloseDB()
 }
