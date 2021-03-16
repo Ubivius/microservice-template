a38d69f2e174e30a5e1f7c0b5f9ed2d20bb9689d
Malcolm St. John <malcolm19@hotmail.com>
2021-03-16 16:50:26 -0400

    updated put request so that it varies more from example post request

--- scripts/curlRequests.sh
+++ scripts/curlRequests.sh
@@ -5,5 +5,5 @@
 curl localhost:9090/products
 curl localhost:9090/products/1
 curl localhost:9090/products -XPOST -d '{"name":"addName", "price":1.00, "sku":"abc-abc-abcd"}'
-curl localhost:9090/products -XPUT -d '{"id":2, "name":"newName", "price":1.00, "sku":"abc-abc-abcd"}'
+curl localhost:9090/products -XPUT -d '{"id":0, "name":"newName", "price":2.00, "sku":"abc-abc-abcde"}'
 curl localhost:9090/products/1 -XDELETE
