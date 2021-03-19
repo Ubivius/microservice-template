357b405fd0da37e2858480cc2dadc86f70a65cac
Malcolm St. John <malcolm19@hotmail.com>
2021-03-18 20:35:29 -0400

    Added comment to clarify logger init

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -4,5 +4,5 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-// MLog is a base parent logger for the controller manager.
+// MLog is a base parent logger for the microservice
 var MLog = logf.Log.WithName("template")
