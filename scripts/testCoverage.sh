da5bda6e9cfeffa2e8662513077244c3d890534a
Malcolm St. John <malcolm19@hotmail.com>
2021-09-14 00:26:21 -0400

    Merged with dev

--- scripts/testCoverage.sh
+++ scripts/testCoverage.sh
@@ -3,3 +3,4 @@
 echo "Running tests and displaying coverage in browser"
 go test ./... -coverprofile=coverage.out -short
 go tool cover -func=coverage.out
+# -html=coverage.out or -func=coverage.out
