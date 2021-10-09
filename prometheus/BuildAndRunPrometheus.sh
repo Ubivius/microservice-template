61fed71de3515d5163ae26c3830c0a149c35b0cc
Malcolm St. John <malcolm19@hotmail.com>
2021-10-09 18:12:09 -0400

    Removed old prometheus config files

--- prometheus/BuildAndRunPrometheus.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t my-prometheus .
-docker run -d -p 60000:9090 my-prometheus
--- prometheus/runPrometheus.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 60000:9090 -v ~/ubivius/microservice-template/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
