51facf6f7c895fa4abe20d3b3aa6e054c94a97df
Malcolm St. John <malcolm19@hotmail.com>
2021-10-10 22:29:34 -0400

    Done mongodb tracing

--- pkg/database/mongoDB.go
+++ pkg/database/mongoDB.go
@@ -11,6 +11,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
 )
 
 // ErrorEnvVar : Environment variable error
@@ -36,17 +37,19 @@ func (mp *MongoProducts) Connect() error {
 	uri := mongodbURI()
 
 	// Setting client options
-	clientOptions := options.Client().ApplyURI(uri)
+	opts := options.Client()
+	clientOptions := opts.ApplyURI(uri)
+	opts.Monitor = otelmongo.NewMonitor()
 
 	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil || client == nil {
 		log.Error(err, "Failed to connect to database. Shutting down service")
 		os.Exit(1)
 	}
 
 	// Ping DB
-	err = client.Ping(context.TODO(), nil)
+	err = client.Ping(context.Background(), nil)
 	if err != nil {
 		log.Error(err, "Failed to ping database. Shutting down service")
 		os.Exit(1)
@@ -63,11 +66,11 @@ func (mp *MongoProducts) Connect() error {
 }
 
 func (mp *MongoProducts) PingDB() error {
-	return mp.client.Ping(context.TODO(), nil)
+	return mp.client.Ping(context.Background(), nil)
 }
 
 func (mp *MongoProducts) CloseDB() {
-	err := mp.client.Disconnect(context.TODO())
+	err := mp.client.Disconnect(context.Background())
 	if err != nil {
 		log.Error(err, "Error while disconnecting from database")
 	}
@@ -78,13 +81,13 @@ func (mp *MongoProducts) GetProducts(ctx context.Context) data.Products {
 	var products data.Products
 
 	// Find returns a cursor that must be iterated through
-	cursor, err := mp.collection.Find(context.TODO(), bson.D{})
+	cursor, err := mp.collection.Find(ctx, bson.D{})
 	if err != nil {
 		log.Error(err, "Error getting products from database")
 	}
 
 	// Iterating through cursor
-	for cursor.Next(context.TODO()) {
+	for cursor.Next(ctx) {
 		var result data.Product
 		err := cursor.Decode(&result)
 		if err != nil {
@@ -98,7 +101,7 @@ func (mp *MongoProducts) GetProducts(ctx context.Context) data.Products {
 	}
 
 	// Close the cursor once finished
-	cursor.Close(context.TODO())
+	cursor.Close(ctx)
 
 	return products
 }
@@ -111,7 +114,7 @@ func (mp *MongoProducts) GetProductByID(ctx context.Context, id string) (*data.P
 	var result data.Product
 
 	// Find a single matching item from the database
-	err := mp.collection.FindOne(context.TODO(), filter).Decode(&result)
+	err := mp.collection.FindOne(ctx, filter).Decode(&result)
 
 	// Parse result into the returned product
 	return &result, err
@@ -128,7 +131,7 @@ func (mp *MongoProducts) UpdateProduct(ctx context.Context, product *data.Produc
 	update := bson.M{"$set": product}
 
 	// Update a single item in the database with the values in update that match the filter
-	_, err := mp.collection.UpdateOne(context.TODO(), filter, update)
+	_, err := mp.collection.UpdateOne(ctx, filter, update)
 	if err != nil {
 		log.Error(err, "Error updating product.")
 	}
@@ -143,7 +146,7 @@ func (mp *MongoProducts) AddProduct(ctx context.Context, product *data.Product) 
 	product.UpdatedOn = time.Now().UTC().String()
 
 	// Inserting the new product into the database
-	insertResult, err := mp.collection.InsertOne(context.TODO(), product)
+	insertResult, err := mp.collection.InsertOne(ctx, product)
 	if err != nil {
 		return err
 	}
@@ -157,7 +160,7 @@ func (mp *MongoProducts) DeleteProduct(ctx context.Context, id string) error {
 	filter := bson.D{{Key: "_id", Value: id}}
 
 	// Delete a single item matching the filter
-	result, err := mp.collection.DeleteOne(context.TODO(), filter)
+	result, err := mp.collection.DeleteOne(ctx, filter)
 	if err != nil {
 		log.Error(err, "Error deleting product")
 	}
