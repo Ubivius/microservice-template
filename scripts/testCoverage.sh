b530e51adc3a28b6467185ca8ea6743979cab885
Malcolm St. John <malcolm19@hotmail.com>
2021-10-10 21:21:51 -0400

    added missing newlines

--- scripts/testCoverage.sh
+++ scripts/testCoverage.sh
@@ -3,4 +3,4 @@
 echo "Running tests and displaying coverage in browser"
 go test ./... -coverprofile=coverage.out -short
 go tool cover -func=coverage.out
-# -html=coverage.out or -func=coverage.out
+# -html=coverage.out to open browser with code coverage or -func=coverage.out to print results to coverage.out
